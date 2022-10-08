4fa5b5fe25a572fd66ec8e4a2807b1aa32741f89
cyyc1 <114281716+cyyc1@users.noreply.github.com>
2022-10-08 12:23:46 -0700

    Add API; add tests; add config

--- jupyter_notebook_parser/parser.py
+++ jupyter_notebook_parser/parser.py
@@ -1,5 +1,5 @@
 import json
-from typing import List, Dict, Optional
+from typing import List, Dict
 
 
 class JupyterNotebookParser:
@@ -7,36 +7,79 @@ class JupyterNotebookParser:
         if not notebook_filename.endswith('.ipynb'):
             raise NameError('This file is not a Jupyter notebook')
 
+        self.notebook_corrupted_msg = 'This notebook file is possibly corrupted'
         try:
             with open(notebook_filename) as fp:
                 notebook_content = json.load(fp)
         except json.JSONDecodeError:
-            raise IOError('Error loading this notebook. File corrupted?')
+            raise ValueError(self.notebook_corrupted_msg)
         else:
             self.notebook_content: Dict = notebook_content
 
-    def get_all_cells(self) -> List[Dict]:
-        return self._get_cells(type_=None)
 
-    def get_code_cells(self) -> List[str]:
+    def get_all_cells(self) -> List[Dict]:
+        if 'cells' not in self.notebook_content:
+            raise ValueError(self.notebook_corrupted_msg)
+
+        return self.notebook_content['cells']
+
+    def get_code_cell_indices(self) -> List[int]:
+        return self._get_cell_indices(type_='code')
+
+    def get_code_cells(self) -> List[Dict]:
         return self._get_cells(type_='code')
 
-    def get_markdown_cells(self) -> List[str]:
+    def get_code_cell_sources(self) -> List[str]:
+        code_cells = self.get_code_cells()
+        return self._join_lines_in_cells(cells=code_cells)
+
+    def get_markdown_cell_indices(self) -> List[int]:
+        return self._get_cell_indices(type_='markdown')
+
+    def get_markdown_cells(self) -> List[Dict]:
         return self._get_cells(type_='markdown')
 
-    def _get_cells(self, type_: Optional[str]) -> List[str]:
-        if 'cells' not in self.notebook_content:
-            raise IOError('This notebook file is possibly corrupted.')
+    def get_markdown_cell_sources(self) -> List[str]:
+        markdown_cells = self.get_markdown_cells()
+        return self._join_lines_in_cells(cells=markdown_cells)
 
-        if type_ is None:
-            return self.notebook_content['cells']
+    def _get_cell_indices(self, type_: str) -> List[int]:
+        self._validate_type_arg(type_)
+        indices_of_code_cells = []
+        for i, cell in enumerate(self.get_all_cells()):
+            if cell.get('cell_type') == type_:
+                indices_of_code_cells.append(i)
 
+        return indices_of_code_cells
+
+    def _get_cells(self, type_: str) -> List[Dict]:
+        self._validate_type_arg(type_)
+        all_cells = self.get_all_cells()
+        return [cell for cell in all_cells if cell['cell_type'] == type_]
+
+    def _join_lines_in_cells(self, cells: List[Dict]) -> List[str]:
+        all_cell_sources = []
+        for cell in cells:
+            self._check_source_in_cell(cell)
+            joined_lines = self._join_source_lines(cell.get('source', ''))
+            all_cell_sources.append(joined_lines)
+
+        return all_cell_sources
+
+    @classmethod
+    def _join_source_lines(
+            cls,
+            lines: List[str],
+            append_newline: bool = False,
+    ) -> str:
+        joined = ''.join(lines)
+        return joined + '\n' if append_newline else joined
+
+    @classmethod
+    def _validate_type_arg(cls, type_: str) -> None:
         if type_ not in {'code', 'markdown'}:
             raise ValueError('Invalid `type` value')
 
-        qualifying_cells = [
-            cell['source']
-            for cell in self.notebook_content['cells']
-            if cell['cell_type'] == type_
-        ]
-        return [''.join(_) for _ in qualifying_cells]
+    def _check_source_in_cell(self, cell: Dict) -> None:
+        if 'source' not in cell:
+            raise ValueError(self.notebook_corrupted_msg)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 from jupyter_notebook_parser import JupyterNotebookParser
 
@@ -6,11 +7,183 @@ THIS_FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 RESOURCE_DIR = os.path.join(THIS_FILE_DIR, 'resources')
 
 
+expected_all_cells = [
+    {
+        'cell_type': 'code',
+        'execution_count': 1,
+        'id': '1a4e6df0',
+        'metadata': {},
+        'outputs': [],
+        'source': [
+            'from typing import List, Dict'
+        ]
+    },
+    {
+        'cell_type': 'code',
+        'execution_count': 2,
+        'id': 'd72a909f',
+        'metadata': {},
+        'outputs': [],
+        'source': [
+            'def some_function(arg1: List[str], arg2: Dict[str, float]):\n',
+            '    print(arg1)\n',
+            '    return arg2'
+        ]
+    },
+    {
+        'cell_type': 'markdown',
+        'id': '0040a4f4',
+        'metadata': {},
+        'source': [
+            '## Section 1'
+        ]
+    },
+    {
+        'cell_type': 'code',
+        'execution_count': 3,
+        'id': '3a1b985c',
+        'metadata': {},
+        'outputs': [
+            {
+                'name': 'stdout',
+                'output_type': 'stream',
+                'text': [
+                    "['a', 'b']\n"
+                ]
+            }
+        ],
+        'source': [
+            "a = some_function(['a', 'b'], {'c': 0.2})"
+        ]
+    },
+    {
+        'cell_type': 'code',
+        'execution_count': 4,
+        'id': '2dfd7d0b',
+        'metadata': {},
+        'outputs': [],
+        'source': [
+            'assert 3 == 3'
+        ]
+    },
+    {
+        'cell_type': 'markdown',
+        'id': 'de5a89c9',
+        'metadata': {},
+        'source': [
+            'Here is a math formula:\n',
+            '\n',
+            '$$c = \\sqrt{a^2 + b^2}$$'
+        ]
+    },
+    {
+        'cell_type': 'code',
+        'execution_count': 5,
+        'id': '4952b6de',
+        'metadata': {},
+        'outputs': [],
+        'source': [
+            'def some_other_function() -> bool:\n',
+            '    """\n',
+            '    Some docstring\n',
+            '    """\n',
+            '    return False'
+        ]
+    },
+    {
+        'cell_type': 'markdown',
+        'id': 'bf1cb851',
+        'metadata': {},
+        'source': [
+            'Some texts.'
+        ]
+    },
+    {
+        'cell_type': 'code',
+        'execution_count': 6,
+        'id': '01224536',
+        'metadata': {},
+        'outputs': [
+            {
+             'name': 'stdout',
+             'output_type': 'stream',
+             'text': [
+              'False\n'
+             ]
+            }
+        ],
+        'source': [
+            'print(some_other_function())'
+        ]
+    },
+    {
+        'cell_type': 'markdown',
+        'id': 'b54d0660',
+        'metadata': {},
+        'source': [
+            '### Some more texts\n',
+            '\n',
+            '| Syntax      | Description |\n',
+            '| ----------- | ----------- |\n',
+            '| Header      | Title       |\n',
+            '| Paragraph   | Text        |'
+        ]
+    },
+    {
+        'cell_type': 'code',
+        'execution_count': 7,
+        'id': 'dc0edb11',
+        'metadata': {},
+        'outputs': [
+            {
+                 'name': 'stdout',
+                 'output_type': 'stream',
+                 'text': [
+                     'abcd"efg".\n'
+                 ]
+            }
+        ],
+        'source': [
+            "print('abcd\"efg\".')"
+        ]
+    }
+]
+
+
+def test_parser_get_all_cells():
+    filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
+    parser = JupyterNotebookParser(filename)
+    all_cells = parser.get_all_cells()
+    assert all_cells == expected_all_cells
+
+
 def test_parser_get_code_cells():
     filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
     parser = JupyterNotebookParser(filename)
     code_cells = parser.get_code_cells()
-    expected_cells = [
+    expected_code_cells = [
+        expected_all_cells[0],
+        expected_all_cells[1],
+        expected_all_cells[3],
+        expected_all_cells[4],
+        expected_all_cells[6],
+        expected_all_cells[8],
+        expected_all_cells[10],
+    ]
+    assert code_cells == expected_code_cells
+
+def test_parser_get_code_cell_indices():
+    filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
+    parser = JupyterNotebookParser(filename)
+    code_cell_indices = parser.get_code_cell_indices()
+    expected_indices = [0, 1, 3, 4, 6, 8, 10]
+    assert code_cell_indices == expected_indices
+
+def test_parser_get_code_cell_sources():
+    filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
+    parser = JupyterNotebookParser(filename)
+    code_sources = parser.get_code_cell_sources()
+    expected_sources = [
         'from typing import List, Dict',
         'def some_function(arg1: List[str], arg2: Dict[str, float]):\n    print(arg1)\n    return arg2',  # noqa: E501
         "a = some_function(['a', 'b'], {'c': 0.2})",
@@ -19,17 +192,52 @@ def test_parser_get_code_cells():
         'print(some_other_function())',
         'print(\'abcd"efg".\')',
     ]
-    assert code_cells == expected_cells
-
+    assert code_sources == expected_sources
 
 def test_parser_get_markdown_cells():
     filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
     parser = JupyterNotebookParser(filename)
-    code_cells = parser.get_markdown_cells()
-    expected_cells = [
+    markdown_cells = parser.get_markdown_cells()
+    expected_markdown_cells = [
+        expected_all_cells[2],
+        expected_all_cells[5],
+        expected_all_cells[7],
+        expected_all_cells[9],
+    ]
+    assert markdown_cells == expected_markdown_cells
+
+def test_parser_get_markdown_cell_indices():
+    filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
+    parser = JupyterNotebookParser(filename)
+    markdown_cell_indices = parser.get_markdown_cell_indices()
+    expected_indices = [2, 5, 7, 9]
+    assert markdown_cell_indices == expected_indices
+
+def test_parser_get_markdown_cell_sources():
+    filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
+    parser = JupyterNotebookParser(filename)
+    markdown_sources = parser.get_markdown_cell_sources()
+    expected_sources = [
         '## Section 1',
         'Here is a math formula:\n\n$$c = \sqrt{a^2 + b^2}$$',  # noqa: W605
         'Some texts.',
         '### Some more texts\n\n| Syntax      | Description |\n| ----------- | ----------- |\n| Header      | Title       |\n| Paragraph   | Text        |',  # noqa: E501
     ]
-    assert code_cells == expected_cells
+    assert markdown_sources == expected_sources
+
+def test_not_ipynb_file():
+    with pytest.raises(NameError):
+        JupyterNotebookParser(os.path.join(RESOURCE_DIR, 'test.txt'))
+
+
+def test_corrupted_ipynb_file_1():
+    with pytest.raises(ValueError):
+        filename = os.path.join(RESOURCE_DIR, 'invalid_notebook_1.ipynb')
+        parser = JupyterNotebookParser(filename)
+        parser.get_markdown_cell_sources()
+
+
+def test_corrupted_ipynb_file_2():
+    with pytest.raises(ValueError):
+        filename = os.path.join(RESOURCE_DIR, 'invalid_notebook_2.ipynb')
+        JupyterNotebookParser(filename)
