7cb621492348058791ad0a6c8891a7167639875b
cyyc1 <114281716+cyyc1@users.noreply.github.com>
2022-10-08 00:26:47 -0700

    Add initial project files

---
+++ jupyter_notebook_parser/parser.py
@@ -0,0 +1,36 @@
+import json
+from typing import List, Dict
+
+
+class JupyterNotebookParser:
+    def __init__(self, notebook_filename: str) -> None:
+        if not notebook_filename.endswith('.ipynb'):
+            raise NameError('This file is not a Jupyter notebook')
+
+        try:
+            with open(notebook_filename) as fp:
+                notebook_content = json.load(fp)
+        except json.JSONDecodeError:
+            raise IOError('Error loading this notebook. File corrupted?')
+        else:
+            self.notebook_content: Dict = notebook_content
+
+    def get_code_cells(self) -> List[str]:
+        return self._get_cells(type_='code')
+
+    def get_markdown_cells(self) -> List[str]:
+        return self._get_cells(type_='markdown')
+
+    def _get_cells(self, type_: str) -> List[str]:
+        if not 'cells' in self.notebook_content:
+            raise IOError('This notebook file is possibly corrupted.')
+
+        if type_ not in {'code', 'markdown'}:
+            raise ValueError('Invalid `type` value')
+
+        qualifying_cells = [
+            cell['source']
+            for cell in self.notebook_content['cells']
+            if cell['cell_type'] == type_
+        ]
+        return [''.join(_) for _ in qualifying_cells]
---
+++ tests/test_parser.py
@@ -0,0 +1,37 @@
+import os
+
+
+THIS_FILE_DIR = os.path.abspath(os.path.dirname(__file__))
+RESOURCE_DIR = os.path.join(THIS_FILE_DIR, 'resources')
+
+
+from jupyter_notebook_parser import JupyterNotebookParser
+
+
+def test_parser_get_code_cells():
+    filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
+    parser = JupyterNotebookParser(filename)
+    code_cells = parser.get_code_cells()
+    expected_cells = [
+        'from typing import List, Dict',
+        'def some_function(arg1: List[str], arg2: Dict[str, float]):\n    print(arg1)\n    return arg2',  # noqa: E501
+        "a = some_function(['a', 'b'], {'c': 0.2})",
+        'assert 3 == 3',
+        'def some_other_function() -> bool:\n    """\n    Some docstring\n    """\n    return False',  # noqa: E501
+        'print(some_other_function())',
+        'print(\'abcd"efg".\')',
+    ]
+    assert code_cells == expected_cells
+
+
+def test_parser_get_markdown_cells():
+    filename = os.path.join(RESOURCE_DIR, 'jupyter_notebook_1.ipynb')
+    parser = JupyterNotebookParser(filename)
+    code_cells = parser.get_markdown_cells()
+    expected_cells = [
+        '## Section 1',
+        'Here is a math formula:\n\n$$c = \sqrt{a^2 + b^2}$$',
+        'Some texts.',
+        '### Some more texts\n\n| Syntax      | Description |\n| ----------- | ----------- |\n| Header      | Title       |\n| Paragraph   | Text        |',  # noqa: E501
+    ]
+    assert code_cells == expected_cells
