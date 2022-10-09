fe5b72363e3d650f6e8619afcd808bb0e6c22355
cyyc1 <114281716+cyyc1@users.noreply.github.com>
2022-10-09 01:12:22 -0700

    Bump version to 0.1.4

--- jupyter_notebook_parser/__init__.py
+++ jupyter_notebook_parser/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from jupyter_notebook_parser.parser import JupyterNotebookParser  # noqa: F401
 from jupyter_notebook_parser.rewriter import JupyterNotebookRewriter  # noqa: F401
