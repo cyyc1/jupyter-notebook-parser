f49a2357ebc79a1e35be4379994fdc70a184d7f8
Michael Donkor <74377231+mdonkorka@users.noreply.github.com>
2022-10-10 17:10:22 +0100

    Added info to setup.py file
    I noticed this file could do with some improvements so I added some key information.

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@ from __future__ import annotations
 
 from setuptools import setup
 
-setup()
+setup(
+    name="jupyter-notebook-parser,
+    description="A Python library to parse cell contents from Jupyter notebooks",
+    author="ccy1",
+    url="https://github.com/cyyc1/jupyter-notebook-parser",
+)
